80a15f0bd087987636fb1e181adbb2be522426d3
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-13 14:02:52 +0000

    header complete

--- App/Screens/HomeScreen/HomeScreen.js
+++ App/Screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AppMapView from './AppMapView'
+import Header from './Header'
 
 const HomeScreen = () => {
   return (
     <View>
+      <View style={styles.headerContainer}>
+        <Header/>
+      </View>
       <AppMapView/>
     </View>
   )
@@ -12,4 +16,12 @@ const HomeScreen = () => {
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContainer:{
+    position:'absolute',
+    zIndex:10,
+    padding:10,
+    width:'100%',
+    paddingHorizontal:20
+  }
+})
